5f3d6ea8ef608600ba7b2d2d09af229dbfa608a5
MrBacon470 <mrbacon470@outlook.com>
2022-12-16 11:54:55 -0600

    more stuff

--- JS/update.js
+++ JS/update.js
@@ -1,6 +1,9 @@
 function updateHTML() {
     //Globals
-    
+    DOMCacheGetOrSet('ironIngotText').innerText = `Iron Ingots: ${formatSci(data.smeltedResourcesStored[0])}`
+    DOMCacheGetOrSet('copperIngotText').innerText = `Copper Ingots: ${formatSci(data.smeltedResourcesStored[1])}`
+    DOMCacheGetOrSet('stoneBrickText').innerText = `Stone Bricks: ${formatSci(data.smeltedResourcesStored[2])}`
+    DOMCacheGetOrSet('steelIngotText').innerText = `Steel Ingots: ${formatSci(data.smeltedResourcesStored[3])}`
     if(data.currentTab === 0) {
         DOMCacheGetOrSet('setToggle0').innerText = data.settingsToggles[0] ? 'Depletable Resources [ON]' : 'Depletable Resources [OFF]'
     }
@@ -9,5 +12,8 @@ function updateHTML() {
     }
     else if(data.currentTab === 2) {
         updateSmeltingHTML()
+    }
+    else if(data.currentTab === 3) {
+        updateManufacturingHTML()
     }
 }
