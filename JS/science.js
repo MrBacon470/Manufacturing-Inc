a6bedc0ed21879a80ad5c838dbe4fd14848235ce
MrBacon470 <mrbacon470@outlook.com>
2022-12-19 10:25:04 -0600

    creating researchs

--- JS/science.js
+++ JS/science.js
@@ -1,15 +1,17 @@
 const dataCardNames = ['Automation Data','Logistics Data','Chemical Data','Utilization Data','Productivity Data','Rocketry Data']
 const dataCardColors = ['--red','--green','--blue','--yellow','--purple','--white2']
-
+//In the Future Add the Science Packs from SE (Biological, Material, Astronomical, Energy & Deep Space I-IV)
 const researchObjs = [
     {
-        name: 'Automation',
+        name: 'Automation I',
+        desc: 'The First Tier of Automating Boring Tasks',
         cost: {aD: D(10), lD: D(0), cD: D(0), uD: D(0), pD: D(0), rD: D(0)},
         reqs: [],
     },
     {
-        name: 'Logistics',
-        cost: {aD: D(20), lD: D(0), cD: D(0), uD: D(0), pD: D(0), rD: D(0)},
+        name: 'Resource Procurement I',
+        desc: 'Begin Producing Raw Materials Automatically with Mk1 Miners',
+        cost: {aD: D(50), lD: D(0), cD: D(0), uD: D(0), pD: D(0), rD: D(0)},
         reqs: ['Automation'],
     },
 ]
